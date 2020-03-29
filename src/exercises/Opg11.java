a676701865e17566dff3d678230a71905df907ce
simo215d <simo215d@edu.easj.dk>
2020-03-29 11:20:43 +0200

    deadlock i now understand :o

--- src/exercises/Opg11.java
+++ src/exercises/Opg11.java
@@ -1,67 +1,32 @@
 package exercises;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class Opg11 {
 
-    Lock lock1 = new ReentrantLock();
-    Lock lock2 = new ReentrantLock();
+    private static Object spoon = new Object();
+    private static Object bowl = new Object();
 
     public static void main(String[] args) {
-        Opg11 opg11 = new Opg11();
-        opg11.start();
-    }
-
-    public void start(){
-        ExecutorService executor = Executors.newCachedThreadPool();
-        System.out.println("program begun");
-        for (int i = 0; i < 10; i++) {
-            executor.execute(new MyTask(1));
-            executor.execute(new MyTask(2));
-        }
-        executor.shutdown();
-        while (!executor.isTerminated()) { }
-        System.out.println("program ended");
-    }
-
-    private class MyTask implements Runnable{
-
-        private int methodToRun;
-
-        MyTask(int n){
-            methodToRun=n;
-        }
-        @Override
-        public void run() {
-            if (methodToRun==1){
-                System.out.println("im "+methodToRun+"!");
-                synchronized (lock1) {
-                    lock2.lock();
-                    System.out.println("thread"+methodToRun + " har fået låst lock2.");
-                    synchronized (lock2) {
-                        lock1.lock();
-                        System.out.println("thread"+methodToRun + " har fået låst lock1.");
-                        lock1.unlock();
-                        lock2.unlock();
-                    }
-                }
-            } else {
-                System.out.println("im "+methodToRun+"!");
-                synchronized (lock2) {
-                    lock1.lock();
-                    System.out.println("thread"+methodToRun + " har fået låst lock2.");
-                    synchronized (lock1) {
-                        lock2.lock();
-                        System.out.println("thread"+methodToRun + " har fået låst lock1.");
-                        lock2.unlock();
-                        lock1.unlock();
-                    }
+        Thread cook1 = new Thread(() -> {
+            synchronized (spoon){
+                System.out.println("cook 1 has locked spoon");
+                System.out.println("cook 1 is waiting for bowl to be unlocked");
+                synchronized (bowl){
+                    System.out.println("cook 1 has locked bowl");
                 }
             }
-        }
+        });
+        Thread cook2 = new Thread(() -> {
+            synchronized (bowl){
+                System.out.println("cook 2 has locked bowl");
+                System.out.println("cook 2 is waiting for spoon to be unlocked");
+                synchronized (spoon){
+                    System.out.println("cook 2 has locked spoon");
+                }
+            }
+        });
+        //try to run the code many times until deadlock occurs
+        //sometimes the second thread is too slow to lock the bowl
+        cook1.start();
+        cook2.start();
     }
 }
